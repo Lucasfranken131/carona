bb944bf27c9f1bfa22243ef0e97ef29d09946281
Ziriell <gabrielmz02@hotmail.com>
2024-02-24 20:28:51 -0300

    alo

--- src/carona_solidaria/cadastro/cadastro.jsx
+++ src/carona_solidaria/cadastro/cadastro.jsx
@@ -8,7 +8,7 @@ const Cadastro = () => {
                 <input type="text" id="nome" placeholder="Nome completo"/>
                 <input type="email" id="email" placeholder="Email"/>
                 <input type="password" id="senha" placeholder="Senha"/>
-                <input type="submit" id="enviar" value="enviar"/>
+                <input type="submit" id="enviar" value="Enviar"/>
             </div>
         </body>
     );
