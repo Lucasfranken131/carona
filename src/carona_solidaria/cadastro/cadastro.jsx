14ec45f39303c13995558459b9592a7aae734d21
Ziriell <gabrielmz02@hotmail.com>
2024-02-26 16:26:55 -0300

    conexão com back-end

--- src/carona_solidaria/cadastro/cadastro.jsx
+++ src/carona_solidaria/cadastro/cadastro.jsx
@@ -4,10 +4,11 @@ const Cadastro = () => {
     return(
         <body>
             <div className="container">
-                <h1>Carona Solidária</h1>
+                <h1>Cadastro</h1>
                 <input type="text" id="nome" placeholder="Nome completo"/>
                 <input type="email" id="email" placeholder="Email"/>
                 <input type="text" id="curso" placeholder="Curso"/>
+                <input type="tel" id="telefone" placeholder="Telefone" pattern="\([0-9]{2}\) [0-9]{5}-[0-9]{4}" required/>
                 <input type="password" id="senha" placeholder="Senha"/>
                 <input type="submit" id="enviar" value="Enviar"/>
             </div>
