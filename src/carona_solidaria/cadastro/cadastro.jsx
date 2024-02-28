0eed9dc8e7bd1f19fcf67dce4a10f71ddb3101e9
Lucas Albuquerque Franco <lu058527@intelbras.com.br>
2024-02-28 16:13:12 -0300

    nem sei

--- src/carona_solidaria/cadastro/cadastro.jsx
+++ src/carona_solidaria/cadastro/cadastro.jsx
@@ -5,6 +5,7 @@ import { useNavigate } from "react-router-dom";
 
 const Cadastro = () => {
     const navigate = useNavigate();
+    const [ hasCar, setHasCar ] = useState(false);
     
     // Estados para rastrear os valores dos campos de entrada
     const [formData, setFormData] = useState({
@@ -30,6 +31,20 @@ const Cadastro = () => {
         });
     };
 
+    const checkCar = () => {
+        if(hasCar == false) {
+            setFormData.car_model = 'Não possui';
+            setFormData.plate = 'Não possui';
+            setHasCar(true)
+
+        }
+        else {
+            setHasCar(true)
+            setFormData(formData.car_model = '');
+            setFormData(formData.plate = '');
+        }
+    }
+ 
     // Função para enviar o formulário
     const handleSubmit = async (e) => {
         e.preventDefault(); // Impede o comportamento padrão do envio do formulário
@@ -51,21 +66,26 @@ const Cadastro = () => {
                 <input type="text" name="cpf" value={formData.cpf} onChange={handleInputChange} placeholder="CPF" required maxLength={14}/>
                 <input type="email" name="email" value={formData.email} onChange={handleInputChange} placeholder="Email" required />
                 <input type="password" name="password" value={formData.password} onChange={handleInputChange} placeholder="Senha" required />
-                <input type="number" name="age" value={formData.age} onChange={handleInputChange} placeholder="Idade" required />
+                <input type="text" name="age" pattern="\d*" maxLength={3} value={formData.age} onChange={handleInputChange} placeholder="Idade" required />
                 <select name="sex" value={formData.sex} onChange={handleInputChange}>
                     <option value="Masculino">Masculino</option>
                     <option value="Feminino">Feminino</option>
                     <option value="Outro">Outro</option>
                     <option value="Prefiro não informar">Prefiro não informar</option>
                 </select>
-                <input type="number" name="phone_number" value={formData.phone_number} onChange={handleInputChange} placeholder="Número de Telefone" required minLength="8" maxLength="8" />
+                <input type="text" name="phone_number" pattern="\d*" value={formData.phone_number} onChange={handleInputChange} placeholder="Número de Telefone" required minLength="8" maxLength="8" />
                 <select name="turn" value={formData.turn} onChange={handleInputChange}>
                     <option value="Matutino">Matutino</option>
                     <option value="Vespertino">Vespertino</option>
                     <option value="Noturno">Noturno</option>
                 </select>
-                <input type="text" name="car_model" value={formData.car_model} onChange={handleInputChange} placeholder="Modelo do veículo" required />
-                <input type="text" name="plate" value={formData.plate} onChange={handleInputChange} placeholder="Placa do Veículo" required />
+                <label>Possui carro:<input type="checkbox" name="hasCar" Checked={formData.hasCar} onClick={() => checkCar()} /></label>
+                {hasCar ? (  // Renderizar os campos relacionados ao carro se hasCar for true
+                    <>
+                        <input type="text" name="car_model" value={formData.car_model} onChange={handleInputChange} placeholder="Modelo do veículo" required />
+                        <input type="text" name="plate" value={formData.plate} onChange={handleInputChange} placeholder="Placa do Veículo" required />
+                    </>
+                ) : (<></>)}
                 <select name="user_type" value={formData.user_type} onChange={handleInputChange}>
                     <option value="Passageiro">Passageiro</option>
                     <option value="Motorista">Motorista</option>
