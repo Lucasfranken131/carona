78926882af5b8cafa70b1b99d25e6353b55a7227
Lucasfranken131 <lucas.franco@aluno.fmpsc.edu.br>
2024-02-28 00:01:04 -0300

    vou dormir

--- src/carona_solidaria/cadastro/cadastro.jsx
+++ src/carona_solidaria/cadastro/cadastro.jsx
@@ -1,79 +1,80 @@
 import './cadastro.css';
 import axios from 'axios';
+import { useState } from 'react';
 import { useNavigate } from "react-router-dom";
 
-
 const Cadastro = () => {
-    const navigate = useNavigate()
+    const navigate = useNavigate();
+    
+    // Estados para rastrear os valores dos campos de entrada
+    const [formData, setFormData] = useState({
+        name: '',
+        cpf: '',
+        email: '',
+        password: '',
+        age: '',
+        sex: 'Masculino',
+        phone_number: '',
+        turn: 'Matutino',
+        car_model: '',
+        plate: '',
+        user_type: 'Passageiro'
+    });
 
-    const Enviar = async () => {    
+    // Função para manipular a mudança nos campos de entrada
+    const handleInputChange = (e) => {
+        const { name, value } = e.target;
+        setFormData({
+            ...formData,
+            [name]: value
+        });
+    };
+
+    // Função para enviar o formulário
+    const handleSubmit = async (e) => {
+        e.preventDefault(); // Impede o comportamento padrão do envio do formulário
+        
         try {
-            const name = document.getElementById("nome").value;
-            const email = document.getElementById("email").value;
-            const password = document.getElementById("senha").value;
-            const age = document.getElementById("idade").value;
-            const genero = document.getElementById("genero").value;
-            const phone_number = document.getElementById("numero").value;
-            const turn = document.getElementById("turno").value;
-            const car_model = document.getElementById("modelo").value;
-            const plate = document.getElementById("placa").value;
-            const user_type = document.getElementById("tipo-de-usuario").value;
-    
-            const valores = {
-                name: name,
-                email: email,
-                password: password,
-                age: age,
-                sex: genero,
-                phone_number: phone_number,
-                turn: turn,
-                car_model: car_model,
-                plate: plate,
-                user_type: user_type
-            }
-
-            await axios.post('http://localhost:3001/usuario', valores)
-            .then(res => {
-              const response = res.data;
-              console.log(response);
-              navigate(`/home`);
-            })
-    
-        }       
-        catch (error) {
-            return(<div>Valores inválidos</div>)
+            await axios.post('http://localhost:3001/usuario/createOne', formData);
+            navigate(`/home`);
+        } catch (error) {
+            console.error('Erro ao enviar o formulário:', error);
         }
-    }
+        console.log(formData)
+    };
 
-    return(
-        <body>
-            <div className="container">
-                <h1>Cadastro</h1>
-                <form id="cadastro">
-                    <input type="text" id="nome" placeholder="Nome completo"/>
-                    <input type="email" id="email" placeholder="Email"/>
-                    <input type="password" id="senha" placeholder="Senha"/>
-                    <input type="number" id="idade" placeholder="Idade"/>
-                    <select id="genero" name="Gênero">
-                        <option value="mas">Masculino</option>
-                        <option value="fem">Feminino</option>
-                        <option value="outro">Outro</option>
-                        <option value="n/a">Prefiro não informar</option>
-                    </select>
-                    <input type="number" id="numero" placeholder='Número de Telefone'/>
-                    <select id="turno" name="Turno">
-                        <option value="mat">Matutino</option>
-                        <option value="ves">Vespertino</option>
-                        <option value="not">Noturno</option>
-                    </select>
-                    <input type='text' id='model' placeholder='Modelo do veículo'/>
-                    <input type='text' id='placa' placeholder='Placa do Veículo'/>
-                    <input type="submit" id="enviar" onClick={() => Enviar()} value="Cadastrar"/>
-                    <div onClick={() => navigate("/login")}>Já possui uma conta?<strong> clique aqui.</strong></div>
-                </form>
-            </div>
-        </body>
+    return (
+        <div className="container">
+            <h1>Cadastro</h1>
+            <form onSubmit={handleSubmit}>
+                <input type="text" name="name" value={formData.name} onChange={handleInputChange} placeholder="Nome completo" required />
+                <input type="text" name="cpf" value={formData.cpf} onChange={handleInputChange} placeholder="CPF" required maxLength={14}/>
+                <input type="email" name="email" value={formData.email} onChange={handleInputChange} placeholder="Email" required />
+                <input type="password" name="password" value={formData.password} onChange={handleInputChange} placeholder="Senha" required />
+                <input type="number" name="age" value={formData.age} onChange={handleInputChange} placeholder="Idade" required />
+                <select name="sex" value={formData.sex} onChange={handleInputChange}>
+                    <option value="Masculino">Masculino</option>
+                    <option value="Feminino">Feminino</option>
+                    <option value="Outro">Outro</option>
+                    <option value="Prefiro não informar">Prefiro não informar</option>
+                </select>
+                <input type="number" name="phone_number" value={formData.phone_number} onChange={handleInputChange} placeholder="Número de Telefone" required minLength="8" maxLength="8" />
+                <select name="turn" value={formData.turn} onChange={handleInputChange}>
+                    <option value="Matutino">Matutino</option>
+                    <option value="Vespertino">Vespertino</option>
+                    <option value="Noturno">Noturno</option>
+                </select>
+                <input type="text" name="car_model" value={formData.car_model} onChange={handleInputChange} placeholder="Modelo do veículo" required />
+                <input type="text" name="plate" value={formData.plate} onChange={handleInputChange} placeholder="Placa do Veículo" required />
+                <select name="user_type" value={formData.user_type} onChange={handleInputChange}>
+                    <option value="Passageiro">Passageiro</option>
+                    <option value="Motorista">Motorista</option>
+                </select>
+                <input type="submit" value="Cadastrar" />
+                <div onClick={() => navigate("/login")}>Já possui uma conta? <strong>clique aqui.</strong></div>
+            </form>
+        </div>
     );
 };
 
-export default Cadastro  
+export default Cadastro;
